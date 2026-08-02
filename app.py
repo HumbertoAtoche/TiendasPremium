ea60115fd7a2b08b878c91e785b7dede28acf1e1
HumbertoAtoche <humberto1098@outlook.com>
2026-08-01 22:23:29 -0500

    app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Tiendas Premium",
+    page_icon="🏪",
+    layout="wide"
+)
+
+st.logo("🏪")
+
+st.title("Sistema Tiendas Premium")
+
+st.write("Bienvenido")
+
+st.sidebar.title("Menú")
